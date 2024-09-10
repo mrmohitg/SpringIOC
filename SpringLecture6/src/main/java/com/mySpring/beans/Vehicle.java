f150074c94b291be03c1470442d4e9599b89dda5
Mohit <mohit11187@gmail.com>
2024-09-10 22:58:06 +0530

    PostConstruct & PreDestroy Annotations

---
+++ SpringLecture6/src/main/java/com/mySpring/beans/Vehicle.java
@@ -0,0 +1,31 @@
+package com.mySpring.beans;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @PostConstruct
+    public void initialize(){
+        this.name = "Audi";
+    }
+
+    public void printHello(){
+        System.out.println("Hello World from Component Vehicle Bean");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Destroying Vehicle Bean.");
+    }
+}
